2094470fafed889a93a255a69bfb9eca4e1baa20
Brandon Stewart <boveus@gmail.com>
2017-07-24 09:12:56 -0600

    Add working test for create photo and retrieve ID.

--- lib/photograph.rb
+++ lib/photograph.rb
@@ -1,2 +1,8 @@
 class Photograph
+  attr_reader :id
+  def initialize(info)
+    @info = info
+    @id = 1
+    @museum_id = info[:museum_id]
+  end
 end
--- test/photograph_test.rb
+++ test/photograph_test.rb
@@ -4,4 +4,34 @@ require 'minitest/pride'
 require './lib/photograph'
 
 class PhotographTest < Minitest::Test
+  def setup
+    @photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+  end
+  def test_create_photograph
+    assert_instance_of Photograph, @photograph
+  end
+  def test_retrieve_id
+    assert_equal 1, @photograph.id
+  end
+  def
+
 end
+
+#
+# photograph = Photograph.new({name: "Rue Mouffetard, Paris (Boy with Bottles)", artist_id: 4, museum_id: 2, year: 1954})
+# # => #<Photograph:0x007f98a9c6ace8 ...>
+#
+# > photograph.id
+# # => 1
+#
+# > photograph.artist_id
+# # => 4
+#
+# > photograph.museum_id
+# # => 2
+#
+# > photograph.name
+# # => "Rue Moffetard, Paris (Boy with Bottles)"
+#
+# > photograph.year
+# # => 1954
